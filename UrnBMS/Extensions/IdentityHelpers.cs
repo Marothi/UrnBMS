47327f935693bae50a562a2fea7bc02e8235f925
Hlakzin <Hlakzin@INTJ-A>
2019-02-27 11:33:25 +0200

    Add project files.

---
+++ UrnBMS/Extensions/IdentityHelpers.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNet.Identity.Owin;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UrnBMS.Extensions
+{
+	public static class IdentityHelpers
+	{
+		public static MvcHtmlString GetUserFullName(this HtmlHelper html, string id)
+		{
+			ApplicationUserManager userManager
+				= HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>();
+
+			return new MvcHtmlString(
+				userManager.FindByIdAsync(id).Result.FirstName 
+				+ userManager.FindByIdAsync(id).Result.LastName);
+		}
+	}
+}
---
+++ UrnBMS/WorkOrderMigrations/201901071919562_BackOrderColumn.cs
@@ -0,0 +1,16 @@
+namespace UrnBMS.WorkOrderMigrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class BackOrderColumn : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
